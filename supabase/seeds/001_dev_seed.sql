e61ac2933781535f1c5d288dbc0c79bb308efe33
NickTheDevOpsGuy <limeman@gmail.com>
2026-01-26 16:02:17 -0500

    Adding in other admins

--- supabase/seeds/001_dev_seed.sql
+++ supabase/seeds/001_dev_seed.sql
@@ -5,5 +5,9 @@
 insert into public.admin_allowlist (email)
 values
   ('limeman@gmail.com'),
-  ('wrdlnkdn@gmail.com')
+  ('wrdlnkdn@gmail.com'),
+  ('aprillordrake@gmail.com'),
+  ('jameshood118@gmail.com'),
+  ('piorana@gmail.com'),
+  ('christopher.s.carter01@gmail.com')
 on conflict (email) do nothing;

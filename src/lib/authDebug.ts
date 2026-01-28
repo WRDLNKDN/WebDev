3cb4d0cb5cc95d47a08586999b5a284e0855d979
NickTheDevOpsGuy <limeman@gmail.com>
2026-01-28 16:33:11 -0500

    chore: auto-fix eslint [skip-precheck]

--- src/lib/authDebug.ts
+++ src/lib/authDebug.ts
@@ -19,10 +19,8 @@ export const authDebug = (
   const prefix = `[AUTH ${ts}]`;
 
   if (data !== undefined) {
-    // eslint-disable-next-line no-console
     console[level](`${prefix} ${message}`, data);
   } else {
-    // eslint-disable-next-line no-console
     console[level](`${prefix} ${message}`);
   }
 };

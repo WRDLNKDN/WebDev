ae972c4c1c167fb8fd88dffceff50b56106528b2
James Hood <jameshood118@gmail.com>
2026-02-03 12:56:54 -0500

    update app and main

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,22 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async'; // The New Controller
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import theme from './theme';
+import theme from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    {/* HelmetProvider must wrap the app to handle async head updates */}
+    <HelmetProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 );
